4104603d78993a1c8fcb91d6fd7fd5bdd9c605e8
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 15:45:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,6 +24,11 @@ def assign_rooms(array)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
